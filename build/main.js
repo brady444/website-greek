9e028c75307bdf979b9f5c0c31af56bb4a5d321d
brady444 <16694009+brady444@users.noreply.github.com>
2025-02-11 08:15:09 -0600

    updated build script

--- build/main.js
+++ build/main.js
@@ -6,36 +6,34 @@ import constants from "./constants";
 // Run the parser
 import parser from "./parser";
 
-export default async () => {
-	// Generate data.js
+// Generate data.js
 
-	const dataPath = path.join(constants.generatedPath, "data.json");
+const dataPath = path.join(constants.generatedPath, "data.json");
 
-	console.log(`Writing ${dataPath}`);
+console.log(`Writing ${dataPath}`);
 
-	await Bun.write(dataPath, JSON.stringify(parser.data, null, "\t"));
+await Bun.write(dataPath, JSON.stringify(parser.data, null, "\t"));
 
-	// Generate pages.js
+// Generate pages.js
 
-	const pagesPath = path.join(constants.generatedPath, "pages.js");
+const pagesPath = path.join(constants.generatedPath, "pages.js");
 
-	let pagesText = "const pages = {};";
+let pagesText = "const pages = {};";
 
-	const pageFiles = fs.readdirSync(constants.pagesPath, { recursive: true });
+const pageFiles = fs.readdirSync(constants.pagesPath, { recursive: true });
 
-	for (let i = 0; i < pageFiles.length; i++) {
-		const page = pageFiles[i].split(".")[0];
+for (let i = 0; i < pageFiles.length; i++) {
+	const page = pageFiles[i].split(".")[0];
 
-		pagesText += `import ${page} from "../pages/${page}";`;
-		pagesText += `pages.${page} = ${page};`;
-	}
+	pagesText += `import ${page} from "../pages/${page}";`;
+	pagesText += `pages.${page} = ${page};`;
+}
 
-	pagesText += "export default pages;";
+pagesText += "export default pages;";
 
-	console.log(`Writing ${pagesPath}`);
+console.log(`Writing ${pagesPath}`);
 
-	await Bun.write(pagesPath, pagesText);
-};
+await Bun.write(pagesPath, pagesText);
 
 // TODO
 // const outputFiles = fs.readdirSync(constants.outputPath);
